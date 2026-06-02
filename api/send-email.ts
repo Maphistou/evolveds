5718cb700328f1e3af3fd112af858ec2c950c0ca
Maphistou <carlosmoralteixeira@gmail.com>
2026-06-02 17:17:07 +0100

    Update send-email.ts

--- api/send-email.ts
+++ api/send-email.ts
@@ -11,7 +11,7 @@ export default async function handler(req: any, res: any) {
 
     try {
         await resend.emails.send({
-            from: 'Evolveds <noreply@evolveds.pt>',
+            from: 'onboarding@resend.dev',
             to: 'comercial@evolveds.pt',
             subject: `Novo pedido de proposta - ${business || name}`,
             html: `
