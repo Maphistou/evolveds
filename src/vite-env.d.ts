5443ceaa9267c17de866b2cce0124accafb3dd37
Maphistou <carlosmoralteixeira@gmail.com>
2026-06-27 18:31:02 +0100

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare module '*.PNG' {
+  const src: string
+  export default src
+}
+declare module '*.PNG?url' {
+  const src: string
+  export default src
+}
